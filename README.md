4013c57c9100f059968c917960eb4ed062cf4c7d
emmanuel-olat <151914436+emmanuel-olat@users.noreply.github.com>
2024-11-25 20:25:55 +0100

    Altschool Assignment

---
+++ README.md
@@ -0,0 +1,17 @@
+### Step 1
+Navigate to altschool-bash-assignment folder
+```bash
+cd altschool-bash-assignment/
+```
+
+### Step 2
+Grant executable and read-write permissions
+```bash
+chmod +x instructor.sh
+```
+
+### Step 2
+Run Script
+```bash
+./instructor.sh
+```
---
+++ instructor.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+who_am_i() {
+  echo "Hello, I am a Cloud Engineer🎉"
+  echo "Full Name 👨🏾: Omotolani Olatunde"
+  echo "Bio 📋: Currently a Backend Engineer, with experience in Spring boot, Java/Kotlin, Docker, MySQL and MongoDB. A tinkerer and passionate about problem solving. Value is the power we hold as human."
+  echo "Years in Tech 📡: 6 years"
+  echo "Dream Job 👨🏾‍💻: Cloud Solutions Architect"
+  echo "Goals ⚽️: Build Systems that connect Africa: I reall believe Africa can become the next internet village of the globe"
+}
+
+who_am_i
+# write bash fucntions that prints out the following infomation about you
+
+# Fullname
+# Bio
+# Year in tech
+# Dream job
+# Goals ⚽
